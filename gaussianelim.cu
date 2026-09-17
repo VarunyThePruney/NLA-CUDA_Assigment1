527593c17c6566e811c8407f964ab588786cfd3d
VarunyThePruney <varungolakoti@gmail.com>
2026-09-17 15:47:33 +0530

    created SDD A matrix rather than random values

--- gaussianelim.cu
+++ gaussianelim.cu
@@ -3,35 +3,56 @@
 #include <cuda_runtime.h>
 #include <math.h>
 
-int rand_num(int min, int max)
+void createSDD(int *a, int n)
 {
-    return (rand() % (max - min + 1) + min);
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        int count = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (i != j)
+            {
+                a[i * n + j] = 0;
+            }
+        }
+
+        while (count < 9)
+        {
+            int j = rand() % n;
+            if (j != i && a[i * n + j] == 0)
+            {
+                int value = (rand() % 2 == 0) ? -1 : 1;
+
+                a[i * n + j] = value;
+                sum += abs(value);
+                count++;
+            }
+        }
+        a[i * n + i] = sum + 1;
+    }
 }
 
 int main()
 {
     srand(time(NULL));
-    int min = -10;
-    int max = 10;
-
-    int sizes[] = {1000, 2000};
+    int sizes[] = {10, 2000};
     int threads[] = {8, 16, 32};
 
-    for (int s = 0; s < 2; s++)
+    for (int s = 0; s < 1; s++)
     {
         int n = sizes[s];
         int size = n * n;
 
         int *a = (int *)malloc(size * sizeof(int));
-        for (int i = 0; i < size; i++)
-        {
-            a[i] = rand_num(min, max);
-        }
+
+        createSDD(a, n);
 
         int *b = (int *)malloc(n * sizeof(int));
         for (int i = 0; i < n; i++)
         {
             b[i] = 100;
+            printf("%d\n", a[i]);
         }
     }
 }
