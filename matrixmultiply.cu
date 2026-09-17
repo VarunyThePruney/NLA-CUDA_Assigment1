883c34266a77f22c1ac0bc9ee33e5201cc30212d
VarunyThePruney <varungolakoti@gmail.com>
2026-09-17 08:58:54 +0530

    feat: added main, operates cpu and gpu methods for varying thread blocks, and calculates speedup for all relations

--- matrixmultiply.cu
+++ matrixmultiply.cu
@@ -24,7 +24,7 @@ void cpuMultiply(float *a, float *c, int n)
     }
 }
 
-__global__ void gpuMutiply(float *a, float *b, float *c, int n)
+__global__ void gpuMutiply(float *a, float *c, int n)
 {
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
@@ -49,5 +49,68 @@ int main()
     {
         int n = sizes[s];
         int size = n * n;
+
+        float *a = (float *)malloc(size * sizeof(float));
+        float *c = (float *)malloc(size * sizeof(float));
+
+        for (int i = 0; i < size; i++)
+        {
+            a[i] = 1;
+            c[i] = 0;
+        }
+
+        double start = time();
+
+        cpuMultiply(a, c, n);
+        double cpu_time = time() - start;
+        printf("Matrix %d x %d:\nCPU time: %f seconds\n\n", n, n, cpu_time);
+
+        float *d_a;
+        float *d_c;
+
+        cudaMalloc(&d_a, size * sizeof(float));
+        cudaMalloc(&d_c, size * sizeof(float));
+
+        cudaMemcpy(d_a, a, size * sizeof(float), cudaMemcpyHostToDevice);
+        double nxn_time[] = {0, 0, 0};
+
+        for (int t = 0; t < 3; t++)
+        {
+            int thread = threads[t];
+
+            dim3 block(thread, thread);
+            dim3 grid((n + thread - 1) / thread, (n + thread - 1) / thread);
+
+            double total = 0;
+
+            printf("Threads per block: %d x %d\n", thread, thread);
+
+            for (int run = 0; run < 5; run++)
+            {
+                cudaMemset(d_c, 0, size * sizeof(float));
+                cudaDeviceSynchronize();
+                start = time();
+
+                gpuMutiply<<<grid, block>>>(d_a, d_c, n);
+                cudaDeviceSynchronize();
+
+                double gpuTime = time() - start;
+                total += gpuTime;
+                printf("run: %d Total time: %f\n", run + 1, gpuTime);
+            }
+
+            double average = total / 5;
+            nxn_time[t] = average;
+            printf("Average Time: %f\n", average);
+            printf("Speedup comparing CPU Time to GPU: %fx\n", cpu_time / average);
+        }
+        printf("Speedup comparing 16x16 to 8x8: %fx\n", nxn_time[0] / nxn_time[1]);
+        printf("Speedup comparing 32x32 to 8x8: %fx\n", nxn_time[0] / nxn_time[2]);
+        printf("Speedup comparing 32x32 to 16x16: %fx\n", nxn_time[1] / nxn_time[2]);
+
+        cudaFree(d_a);
+        cudaFree(d_c);
+        free(a);
+        free(c);
     }
 }
